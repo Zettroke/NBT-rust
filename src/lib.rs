69cf7f9f1877d33d7f4879365ab4bc88b911d057
Zettroke <skorostnoy2000@yandex.ru>
2019-11-22 22:12:53 +0300

    WIP

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,34 @@
 
 
 pub mod nbt {
+    extern crate bytes;
     use std::collections::HashMap;
-    use std::io::{Read, Error};
+    use std::io::{Read, Error, ErrorKind};
+    use std::convert::TryInto;
+    use std::iter::Map;
+    use bytes::BigEndian;
+    use self::bytes::ByteOrder;
 
     #[derive(Debug)]
-    pub enum NBTPayload{
+    pub enum NBT{
         Byte(u8),
-        Short(u16),
-        Int(u32),
-        Long(u64),
+        Short(i16),
+        Int(i32),
+        Long(i64),
         Float(f32),
         Double(f64),
         ByteArray(Vec<u8>),
         String(String),
-        List(Vec<NBTPayload>),
-        Compound(HashMap<String, Box<NBT>>)
+        List(Vec<NBT>),
+        Compound(HashMap<String, NBT>)
 
     }
 
-    #[derive(Debug)]
-    pub struct NBT {
-        pub name: String,
-        pub data: NBTPayload
-    }
+//    #[derive(Debug)]
+//    pub struct NBT {
+//        pub name: String,
+//        pub data: NBT
+//    }
 
     pub struct ReadWrapper<T: Read> {
         inner: T
@@ -45,7 +50,7 @@ pub mod nbt {
             {
                 let mut buff = vec![0; 2];
                 self.read(&mut buff[0..2]);
-                n = u16::from_be_bytes([buff[0], buff[1]]) as usize;
+                n = BigEndian::read_u16(&mut buff) as usize;
             }
 
             let mut buff = vec![0; n];
@@ -53,35 +58,35 @@ pub mod nbt {
             return String::from_utf8(buff).unwrap();
         }
 
-        pub fn readPlainNBTPayload(&mut self, t: u8, buff: &mut Vec<u8>, buff_size: usize) -> NBTPayload {
+        pub fn readPlainNBT(&mut self, t: u8, buff: &mut Vec<u8>, buff_size: usize) -> NBT {
             match t {
                 0x01 => { // Byte
                     self.read_n(buff, 1);
-                    NBTPayload::Byte(buff[0])
+                    NBT::Byte(buff[0])
                 },
                 0x02 => { // Short
                     self.read_n(buff, 2);
-                    NBTPayload::Short((buff[0] as u16) << 8 | buff[1] as u16)
+                    NBT::Short(BigEndian::read_i16(buff))
                 },
                 0x03 => { // Int
                     self.read_n(buff, 4);
-                    NBTPayload::Int(u32::from_be_bytes([buff[0], buff[1], buff[2], buff[3]]))
+                    NBT::Int(BigEndian::read_i32(buff))
                 },
                 0x04 => { // Long
                     self.read_n(buff, 8);
-                    NBTPayload::Long(u64::from_be_bytes([buff[0], buff[1], buff[2], buff[3], buff[4], buff[5], buff[6], buff[7]]))
+                    NBT::Long(BigEndian::read_i64(buff))
                 },
                 0x05 => { // Float
                     self.read_n(buff, 4);
-                    NBTPayload::Float(f32::from_be_bytes([buff[0], buff[1], buff[2], buff[3]]))
+                    NBT::Float(BigEndian::read_f32(buff))
                 },
                 0x06 => { // Double
                     self.read_n(buff, 8);
-                    NBTPayload::Double(f64::from_be_bytes([buff[0], buff[1], buff[2], buff[3], buff[4], buff[5], buff[6], buff[7]]))
+                    NBT::Double(BigEndian::read_f64(buff))
                 },
                 0x07 => { // Byte Array
                     self.read_n(buff, 4);
-                    let n = u32::from_be_bytes([buff[0], buff[1], buff[2], buff[3]]) as usize;
+                    let n = BigEndian::read_u32(buff) as usize;
                     let step = n / buff_size;
                     let mut res: Vec<u8> = Vec::new();
                     for i in 0..step {
@@ -92,12 +97,12 @@ pub mod nbt {
                     self.read_n(buff,  last_part);
                     res.extend_from_slice(&mut buff[0..last_part]);
 
-                    NBTPayload::ByteArray(res)
+                    NBT::ByteArray(res)
                 },
                 0x08 => {
-                    NBTPayload::String(self.read_name())
+                    NBT::String(self.read_name())
                 },
-                _ => NBTPayload::String(String::from("Invalid tag!!!"))
+                _ => NBT::String(String::from("Invalid tag!!!"))
             }
         }
     }
@@ -109,13 +114,20 @@ pub mod nbt {
     }
 
     impl NBT {
-
+        pub fn as_compound(&mut self) -> Result<&mut HashMap<String, NBT>, Error>{
+            match self {
+                NBT::Compound(v) => Result::Ok(v),
+                _ => Result::Err(Error::new(ErrorKind::Other, "not a compound"))
+            }
+        }
 
         pub fn new() {
 
         }
         pub fn from(input: &mut impl Read, buff_size: usize) {
             let buff_size = 65536;
+
+            let mut stack: Vec<NBT> = vec![NBT::Compound(HashMap::new())];
             let mut src = ReadWrapper::from(input);
             let mut bufff = vec![0u8; buff_size];
             let buff = &mut bufff;
@@ -126,25 +138,28 @@ pub mod nbt {
                 let name = src.read_name();
                 let v = match t {
                     0x01..=0x08 => {
-                        src.readPlainNBTPayload(t, buff, buff_size)
+                        src.readPlainNBT(t, buff, buff_size)
                     },
                     0x09 => { // List
                         src.read_n(buff, 1);
                         let list_t = buff[0];
                         src.read_n(buff, 4);
                         let n = u32::from_be_bytes([buff[0], buff[1], buff[2], buff[3]]) as usize;
-                        let mut res: Vec<NBTPayload> = Vec::with_capacity(n);
+                        let mut res: Vec<NBT> = Vec::with_capacity(n);
                         for i in 0..n {
-                            res.push(src.readPlainNBTPayload(list_t, buff, buff_size));
+                            res.push(src.readPlainNBT(list_t, buff, buff_size));
                         }
 
-                        NBTPayload::List(res)
+                        NBT::List(res)
                     }, // Compound
                     0x10 => {
-                        NBTPayload::String(String::from("lul"))
+                        NBT::String(String::from("lul"))
                     }
-                    _ => NBTPayload::String(String::from("Invalid tag!!!"))
+                    _ => NBT::String(String::from("Invalid tag!!!"))
                 };
+
+                let mut parent: &mut HashMap<String, NBT> = stack.last_mut().unwrap().as_compound().unwrap();
+                parent.insert(name, v);
             }
 
         }
@@ -155,25 +170,26 @@ pub mod nbt {
 #[cfg(test)]
 mod tests {
     use std::fs::File;
-    use crate::nbt::{NBT, NBTPayload};
+    use crate::nbt::{NBT};
     use std::collections::HashMap;
 
     #[test]
     fn it_works() {
         let mut f = File::open("Bikini Bottom V2/level").unwrap();
         let mut buff = vec![0u8; 16];
-        let s = String::from("Kappa");
-        let v = NBT {
-            name: s.clone(),
-            data: NBTPayload::Byte(12u8)
-        };
-        let mut m: HashMap<String, Box<NBT>> = HashMap::new();
-        m.insert(s, Box::from(v));
-        let mut n = NBT {
-            name: String::from("dasdsa"),
-            data: NBTPayload::Compound(m)
-        };
+        NBT::from(&mut f, 65536);
+//        let s = String::from("Kappa");
+//        let v = NBT {
+//            name: s.clone(),
+//            data: NBT::Byte(12u8)
+//        };
+//        let mut m: HashMap<String, Box<NBT>> = HashMap::new();
+//        m.insert(s, Box::from(v));
+//        let mut n = NBT {
+//            name: String::from("dasdsa"),
+//            data: NBT::Compound(m)
+//        };
 
-        println!("{:?}", n);
+//        println!("{:?}", n);
     }
 }
